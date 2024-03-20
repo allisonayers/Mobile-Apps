805804ad1232cc140bdc444af3328a809baabd40
allison ayers <aayerss32@gmail.com>
2024-03-19 23:16:15 -0400

    done

---
+++ News/App.js
@@ -0,0 +1,179 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import USNewsScreen from './screens/USNewsScreen';
+import WorldNewsScreen from './screens/WorldNewsScreen';
+import FashionNewsScreen from './screens/FashionNewsScreen';
+import NewsDetailScreen from './screens/NewsDetailScreen';
+import Colors from "./constants/colors";
+import { useCallback } from "react";
+import BookmarksScreen from "./screens/BookmarkScreen";
+import { Entypo } from "@expo/vector-icons";
+import { FontAwesome6 } from '@expo/vector-icons';
+
+const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
+const Tabs = createBottomTabNavigator();
+
+function DrawerNavigator() {
+  return (
+    <Drawer.Navigator
+      initialRouteName="Listings"
+      screenOptions={{
+        headerStyle: { backgroundColor: Colors.primary500 },
+        headerTintColor: "white",
+        headerTitleStyle: {
+          fontFamily: "playfair",
+          fontSize: 40,
+          color: Colors.accent800,
+        },
+        sceneContainerStyle: { backgroundColor: Colors.primary300 },
+        drawerContentStyle: { backgroundColor: Colors.primary500 },
+        drawerInactiveTintColor: Colors.primary300,
+        drawerActiveTintColor: Colors.accent300,
+        drawerActiveBackgroundColor: Colors.primary800,
+      }}
+    >
+      <Drawer.Screen
+        name="Listings"
+        component={TabsNavigator}
+        options={{
+          title: "All News Listings",
+          drawerLabel: "News Reports",
+          drawerIcon: ({ color, size }) => (
+            <Entypo name="list" size={size} color={color} />
+          ),
+        }}
+      />
+      <Drawer.Screen
+        name="BookmarkScreen"
+        component={BookmarksScreen}
+        options={{
+          title: "Saved News Reports",
+          drawerLabel: "Saved Reports",
+          drawerIcon: ({ color, size }) => (
+            <Entypo name="bookmarks" size={size} color={color} />
+          ),
+        }}
+      />
+    </Drawer.Navigator>
+  );
+}
+
+function TabsNavigator() {
+  return (
+    <Tabs.Navigator
+      screenOptions={{
+        tabBarShowLabel: true,
+        tabBarActiveBackgroundColor: Colors.primary900,
+        tabBarActiveTintColor: Colors.accent800,
+        tabBarInactiveBackgroundColor: Colors.primary500,
+        tabBarInactiveTintColor: Colors.primary300,
+        tabBarLabelStyle: {
+          fontSize: 12,
+          fontFamily: "playfairBold",
+        },
+        tabBarStyle: {
+          backgroundColor: Colors.primary500,
+        },
+      }}
+    >
+      <Tabs.Screen
+        name="USNews"
+        component={USNewsScreen}
+        options={{
+          headerShown: false,
+          tabBarIcon: ({ color, size }) => (
+            <FontAwesome6 name="flag-usa" size={size} color={color} />
+          ),
+          tabBarLabel: "U.S News",
+        }}
+      />
+      <Tabs.Screen
+        name="WorldNews"
+        component={WorldNewsScreen}
+        options={{
+          headerShown: false,
+          tabBarIcon: ({ color, size }) => (
+            <FontAwesome6 name="earth-americas" size={size} color={color} />
+          ),
+          tabBarLabel: "World News",
+        }}
+      />
+      <Tabs.Screen
+        name="FashionNews"
+        component={FashionNewsScreen}
+        options={{
+          headerShown: false,
+          tabBarIcon: ({ color, size }) => (
+            <FontAwesome6 name="shirt" size={size} color={color} />
+          ),
+          tabBarLabel: "Fashion News",
+        }}
+      />
+    </Tabs.Navigator>
+  );
+}
+
+export default function App() {
+  const [fontsLoaded, fontError] = useFonts({
+    playfair: require("./assets/fonts/Playfair.ttf"),
+    playfairBold: require("./assets/fonts/PlayfairBold.ttf"),
+    playfairItalic: require("./assets/fonts/PlayfairBoldItalic.ttf"),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded || fontError) {
+      await SplashScreen.hideAsync();
+    }
+  });
+
+  if (!fontsLoaded && !fontError) {
+    return null;
+  } else {
+    return (
+      <>
+        <StatusBar style="dark" />
+        <NavigationContainer>
+          <Stack.Navigator
+            initialRouteName="DrawerScreen"
+            screenOptions={{
+              headerTintColor: Colors.primary300,
+              headerStyle: { backgroundColor: Colors.primary500 },
+              contentStyle: { backgroundColor: "black" },
+            }}
+          >
+            <Stack.Screen
+              name="DrawerScreen"
+              component={DrawerNavigator}
+              options={{
+                headerShown: false,
+              }}
+            />
+            <Stack.Screen
+              name="NewsDetail"
+              component={NewsDetailScreen}
+              options={{
+                headerBackTitleVisible: false,
+              }}
+            />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
---
+++ News/data/dummy_data.js
@@ -0,0 +1,140 @@
+import Listing from "../models/listing";
+
+export const LISTINGS = [
+  new Listing(
+    1,
+    "US",
+    "New Tech Hub Initiative Announced by White House",
+    "April 5th, 2024",
+    2.5, //ignore
+    2000, //ingnore
+    "CNN",
+    "Alex Johnson",
+    "Charleston", //ignore
+    "SC", //ignore
+    "29401", //ignore
+    "https://media-cldnry.s-nbcnews.com/image/upload/rockcms/2023-10/231023-joe-biden-al-0930-3adaa5.jpg",
+    "The White House has unveiled a new initiative aimed at transforming several cities into tech hubs. The initiative seeks to foster innovation, create jobs, and ensure technological advancements are evenly distributed across the nation."
+  ),
+  new Listing(
+    2,
+    "World",
+    "European Union Expands with New Member States",
+    "April 15th, 2024",
+    2.5, //ignore
+    2000, //ingnore
+    "Al Jazeera",
+    "Marco Gutiérrez",
+    "Charleston", //ignore
+    "SC", //ignore
+    "29401", //ignore
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/4/44/Europe_orthographic_Caucasus_Urals_boundary_%28with_borders%29.svg/220px-Europe_orthographic_Caucasus_Urals_boundary_%28with_borders%29.svg.png",
+    "The European Union has officially welcomed two new member states, marking a significant moment in the history of the EU. This expansion signifies a commitment to unity and strength in the face of global challenges, as well as the potential for economic and social development within the region."
+  ),
+  new Listing(
+    3,
+    "Fashion",
+    "The Met Gala 2024: A Night of Stars and Statements",
+    "May 1st, 2024",
+    2.5, //ignore
+    2000, //ingnore
+    "Vogue",
+    "Sophia Loren",
+    "Charleston", //ignore
+    "SC", //ignore
+    "29401", //ignore
+    "https://assets.vogue.com/photos/654bb1fa34c27ecfb7f315d1/master/w_2560%2Cc_limit/GettyImages-1486966844.jpg",
+    "The Met Gala 2024, fashion's biggest night out, did not disappoint, with celebrities and designers alike making bold statements on the red carpet. This year's theme, 'Icons of the Age', saw an array of breathtaking outfits inspired by the biggest icons in history, from Cleopatra to David Bowie."
+  ),
+  new Listing(
+    4,
+    "US",
+    "Historic Healthcare Reform Bill Passed",
+    "April 20th, 2024",
+    2.5, //ignore
+    2000, //ingnore
+    "Fox News",
+    "Samantha Roe",
+    "Charleston", //ignore
+    "SC", //ignore
+    "29401", //ignore
+    "https://siouxlandchamber.com/wp-content/uploads/healthcarereform2009-09-08-1252412141.jpg",
+    "In a landmark move, Congress has passed a comprehensive healthcare reform bill, which promises to expand coverage, reduce costs, and improve the quality of care for millions of Americans. The President is expected to sign the bill into law next week."
+  ),
+  new Listing(
+    5,
+    "World",
+    "Peace Talks Resume in Middle East",
+    "March 30th, 2024",
+    2.5, //ignore
+    2000, //ingnore
+    "BBC",
+    "Nora Feldman",
+    "Charleston", //ignore
+    "SC", //ignore
+    "29401", //ignore
+    "https://image.cnbcfm.com/api/v1/image/107322815-1698232166266-gettyimages-1722805969-AA_13102023_1375096.jpeg?v=1698232229&w=929&h=523&vtcrop=y",
+    "After months of stalemate, peace talks between rival factions in the Middle East have resumed, thanks to the mediation efforts of the United Nations. The talks aim to bring an end to years of conflict and establish a lasting peace in the region."
+  ),
+  new Listing(
+    6,
+    "Fashion",
+    "Fashion Week Paris: The Rise of Sustainable Couture",
+    "September 25th, 2024",
+    2.5, //ignore
+    2000, //ingnore
+    "Elle",
+    "Geneviève Boucher",
+    "Charleston", //ignore
+    "SC", //ignore
+    "29401", //ignore
+    "https://image-cdn.hypb.st/https%3A%2F%2Fhypebeast.com%2Fwp-content%2Fblogs.dir%2F6%2Ffiles%2F2023%2F01%2FMARINE-SERRE_Rising-Shelter_03.jpg?cbr=1&q=90",
+    "This year's Paris Fashion Week made a clear statement: sustainable fashion is not just a trend, it's the future. Designers showcased collections that emphasized eco-friendly materials, ethical manufacturing practices, and innovative designs that prove fashion can be both beautiful and responsible."
+  ),
+  new Listing(
+    7,
+    "US",
+    "Major Breakthrough in Renewable Energy",
+    "May 15th, 2024",
+    2.5, //ignore
+    2000, //ingnore
+    "NBC",
+    "Liam Crest",
+    "Charleston", //ignore
+    "SC", //ignore
+    "29401", //ignore
+    "https://arayara.org/wp-content/uploads/2023/08/DreamShaper_v7_renewable_energy_industry_making_big_money_0-1.jpg",
+    "Researchers at the University of California have announced a major breakthrough in renewable energy technology, which could significantly reduce the cost of solar power. Experts say this development could accelerate the shift towards cleaner energy sources."
+  ),
+  new Listing(
+    8,
+    "World",
+    "Global Effort to Combat Climate Change Intensifies",
+    "April 22nd, 2024",
+    2.5, //ignore
+    2000, //ingnore
+    "Al Jazeera",
+    "Mohammed Al-Sayed",
+    "Charleston", //ignore
+    "SC", //ignore
+    "29401", //ignore
+    "https://res.cloudinary.com/jerrick/image/upload/d_642250b563292b35f27461a7.png,f_jpg,fl_progressive,q_auto,w_1024/65026d8e03573f001d55acfb.jpg",
+    "Leaders from around the world have convened at the International Climate Summit in Paris to discuss new strategies for combating climate change. The summit has seen unprecedented cooperation between nations in their commitment to reduce greenhouse gas emissions."
+  ),
+  new Listing(
+    9,
+    "Fashion",
+    "Revolutionary Sustainable Fabric Unveiled",
+    "March 25th, 2024",
+    2.5, //ignore
+    2000, //ingnore
+    "Vogue",
+    "Isabella Martin",
+    "Charleston", //ignore
+    "SC", //ignore
+    "29401", //ignore
+    "https://textilefocus.com/wp-content/uploads/sustainable-fabrics.jpg",
+    "At the Milan Fashion Week, a new sustainable fabric that promises to revolutionize the fashion industry was unveiled. Made from recycled materials, the fabric is both eco-friendly and durable, setting a new standard for sustainable fashion."
+  ),
+
+];
